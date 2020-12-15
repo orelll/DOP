1fdbbf9bbe227d9c0143947f2550ed52c4037488
Szymon Orlik <szymon.orlik@capgemini.com>
2020-12-15 16:39:50 +0100

    updated router styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DOP';
 
   pageName: string;
 
+  constructor(private location: Location) {}
+  ngOnInit(): void {
+    this.updateSelectedPage();
+  }
+
   setCurrentPageName(name: string): void {
     this.pageName = name;
+  }
+
+  private updateSelectedPage() {
+    const href = this.location.path(false);
+    if (href.includes('issues')) {
+      this.pageName = 'issues';
+    } else if (href.includes('unprocessed')) {
+      this.pageName = 'unprocessed';
+    }
   }
 }
